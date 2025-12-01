2ca5fef7144d9023ffabed3c2b3973b7955598c9
kerolos <kokoessa951@gmail.com>
2025-12-01 19:43:13 +0200

    feat: add MessageSkeleton component and update ChatComponent to display loading state; enhance MessageInput component

--- frontend/src/components/ChatComponent.jsx
+++ frontend/src/components/ChatComponent.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import Chatheader from "./Chatheader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatComponent = () => {
   const { isMessageLoading, getMessages, selectedUser } = useChatStore();
@@ -9,11 +11,20 @@ const ChatComponent = () => {
     if (selectedUser) getMessages(selectedUser._id);
   }, [selectedUser, getMessages]);
 
-  if (isMessageLoading) return <div>Loading...</div>;
+  if (isMessageLoading)
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <Chatheader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <Chatheader />
+      <p>Messages</p>
+      <MessageInput />
     </div>
   );
 };
--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MessageInput = () => {
-  return <div></div>;
+  return <div>MessageInput</div>;
 };
 
 export default MessageInput;
